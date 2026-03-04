7c6545a3e18b41610ff583698aac25f30f478cee
FarhatulZannat <ebinfaria@gmail.com>
2026-03-05 00:29:30 +0600

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import { Banner } from './ui/banner/banner'
 import { Navbar } from './ui/navbar'
 import { Tickets } from './ui/tickets/tickets'
+import { Footer } from './ui/footer'
 // import { use } from 'react'
 
 
@@ -18,7 +19,8 @@ function App() {
   
 
   return (
-   <>
+   <div className='bg-[#f3f6f8] min-h-screen'>
+    
    <Navbar></Navbar>
 
    <Banner></Banner>
@@ -26,11 +28,10 @@ function App() {
    <Suspense fallback={<div className='flex justify-center items-center '><span class="loading loading-ring loading-xl"></span><span class="loading loading-ring loading-xl"></span><span class="loading loading-ring loading-xl"></span></div>}>
     <Tickets playerPromise={playerPromise}></Tickets>
    </Suspense>
-   
 
-
+   <Footer></Footer>
    
-   </>
+   </div>
     
   )
 }
